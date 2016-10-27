5d0ad50b9d8fbc9c9d05229a61e5f6ec8a621129
Zijun Chen <aprilc401@gmail.com>
2016-10-27 13:48:51 -0700

    Styled map

--- js/map.js
+++ js/map.js
@@ -1,13 +1,30 @@
 function initMap() {
-   var campus = {lat: 32.8811083, lng: -117.2375732};
-   var map = new google.maps.Map(document.getElementById('map'), {
+   // Map options
+   var mapOptions = {
       zoom: 15,
-      center: campus
-       
-       //May styling
-       
-   });
+      center: new google.maps.LatLng(32.8811083, -117.2375732),
+      styles:
+      [{"featureType":"all","elementType":"labels.text.fill",
+      "stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},
+      {"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill",
+      "stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke",
+      "stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},
+      {"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},
+      {"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},
+      {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},
+      {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},
+      {"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},
+      {"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},
+      {"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},
+      {"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all",
+      "stylers":[{"color":"#021019"}]}]
+   };
 
+   var mapElement = document.getElementById('map');
+
+   // Create the map
+   var map = new google.maps.Map(mapElement, mapOptions);
+    
    // Create the search box and link it to the UI element.
    var input = document.getElementById('pac-input');
    var searchBox = new google.maps.places.SearchBox(input);
@@ -45,4 +62,6 @@ function initMap() {
    marker.addListener('click', function() {
       infowindow.open(map, marker);
    });
+    
+    
 }
