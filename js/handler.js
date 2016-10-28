edf02e9826d6ba81e64f49c50045df148d7f45a2
Sanjeev Reddy <sanjeev96red@gmail.com>
2016-10-27 17:18:49 -0700

    Added basic handler for clicking on Find Parking

---
+++ js/handler.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+   initializePage();
+});
+
+function initializePage() {
+   $('#find-park').click(function(){
+      var menu = document.getElementById('permit-select');
+      var type = menu.value;
+      var search = document.getElementById('pac-input');
+      var loc = search.value;
+
+      if(type == "" && loc == "") {
+         alert("Please select a destination and your permit type");
+      } else if(type == "") {
+         alert("Please select your permit type");
+      } else if(loc == "") {
+         alert("Please select your destination on campus");
+      }
+   });
+}
