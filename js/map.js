b005e8f9250c87f8a368291e355c9bfc52aae2a5
sulee9512 <sul032@ucsd.edu>
2016-11-03 11:16:47 -0700

    Moved search box and trying to move select permit type and find parking button

--- js/map.js
+++ js/map.js
@@ -30,7 +30,7 @@ function initMap() {
    // Create the search box and link it to the UI element.
    var input = document.getElementById('pac-input');
    var searchBox = new google.maps.places.SearchBox(input);
-   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+   //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
    // Bias the SearchBox results towards current map's viewport.
    map.addListener('bounds_changed', function() {
