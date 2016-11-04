0eb01b8995cdefb3e9f4b4e66e6d20cb2184983e
Zijun Chen <aprilc401@gmail.com>
2016-11-03 22:24:23 -0700

    add the new login page

--- js/map.js
+++ js/map.js
@@ -6,20 +6,7 @@ function initMap() {
       zoom: 15,
       center: campus,
       styles:
-      [{"featureType":"all","elementType":"labels.text.fill",
-      "stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},
-      {"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill",
-      "stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke",
-      "stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},
-      {"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},
-      {"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},
-      {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},
-      {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},
-      {"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},
-      {"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},
-      {"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},
-      {"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all",
-      "stylers":[{"color":"#021019"}]}]
+   [{"featureType":"water","stylers":[{"color":"#19a0d8"}]},{"featureType":"administrative","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"},{"weight":6}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#e85113"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efe9e4"},{"lightness":-40}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#efe9e4"},{"lightness":-20}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"lightness":-100}]},{"featureType":"road.highway","elementType":"labels.icon"},{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape","stylers":[{"lightness":20},{"color":"#efe9e4"}]},{"featureType":"landscape.man_made","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"lightness":-100}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"hue":"#11ff00"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"hue":"#4cff00"},{"saturation":58}]},{"featureType":"poi","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#f0e4d3"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#efe9e4"},{"lightness":-25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#efe9e4"},{"lightness":-10}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"simplified"}]}]
    };
 
    var mapElement = document.getElementById('map');
